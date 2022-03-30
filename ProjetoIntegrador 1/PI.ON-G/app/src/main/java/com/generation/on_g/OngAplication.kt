3143f37034272ad5ba83dee62185d97e7ce7d969
uGuiihz <guilhermebconte@hotmail.com>
2022-03-30 17:10:20 -0300

    Layout do Login e Registro

--- ProjetoIntegrador 1/PI.ON-G/app/src/main/java/com/generation/on_g/OngAplication.kt
+++
@@ -1,8 +0,0 @@
-package com.generation.on_g
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class OngAplication : Application() {
-}
--- ProjetoIntegrador 1/PI.ON-G/app/src/main/java/com/generation/on_g/util/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.generation.on_g.util
-
-object Constants {
-
-    const val BASE_URL = "https://mobileredequatro.herokuapp.com/"
-}
